02733cfd98ebe38c705cf080976e185fbd5c35e5
Gregory Anne <englishblazere@free.fr>
2019-06-10 12:38:23 +0200

    first implementation of PromptResolver.java

--- spring-shell-prompt-commands/src/main/java/org/springframework/shell/prompt/commands/PromptResolver.java
+++ spring-shell-prompt-commands/src/main/java/org/springframework/shell/prompt/commands/PromptResolver.java
@@ -16,17 +16,21 @@
 package org.springframework.shell.prompt.commands;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+import org.jline.reader.History;
+import org.jline.utils.ExecHelper;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 class NotYetImplementedException extends RuntimeException {
-    
+
 }
 
 /**
  * all the yes/no and 1,2,3 answers implemented here
- * 
- * 
+ *
  * @author Gregory Anne
  */
 @ShellComponent
@@ -34,31 +38,61 @@ public class PromptResolver {
 
     public interface Command {
     }
-    
-    private final org.jline.reader.History jLineHistory;
 
-    public PromptResolver(org.jline.reader.History jLineHistory) {
+    private final History jLineHistory;
+
+    public PromptResolver(History jLineHistory) {
         this.jLineHistory = jLineHistory;
     }
 
     @ShellMethod(value = "reply yes to the last command")
-    public String yes() throws IOException {
-        throw new NotYetImplementedException();
+    public String yes() throws IOException, IllegalAccessException,
+            IllegalArgumentException, InvocationTargetException {
+        List<String> previousCmd = Arrays.asList(this.jLineHistory.get(this.jLineHistory.last()).split(" "));
+        previousCmd.add("--no-prompt");
+        return ExecHelper.exec(false, previousCmd.toArray(new String[previousCmd.size()]));
+//        Map<String, Object> beans = appContext.getBeansWithAnnotation(ShellComponent.class);
+//        Method toInvoke = null;
+//        Object beanToInvoke = null;
+//        for (Map.Entry<String, Object> bean : beans.entrySet()) {
+//            Class zz = bean.getValue().getClass();
+//            for (Method m : zz.getMethods()) {
+//                String[] specifiedKeys = new String[]{m.getName()};
+//                if (m.isAnnotationPresent(ShellMethod.class)) {
+//                    ShellMethod sm = m.getDeclaredAnnotation(ShellMethod.class);
+//                    if (sm.key() != null && sm.key().length > 0) {
+//                        specifiedKeys = sm.key();
+//                    }
+//                    Arrays.sort(specifiedKeys);
+//                    if (Arrays.binarySearch(specifiedKeys, previousCmdName) > -1){
+//                        toInvoke = m;
+//                        beanToInvoke = bean.getValue();
+//                    }
+//                }
+//            }
+//        }
+//        if (toInvoke != null){
+//            toInvoke.invoke(beanToInvoke, previousArgs);
+//        }
     }
 
     @ShellMethod(value = "reply y")
-    public String y() throws IOException {
+    public String y() throws IOException, IllegalAccessException,
+            IllegalArgumentException, InvocationTargetException {
         return yes();
     }
 
     @ShellMethod(value = "reply no to the last command")
     public String no() throws IOException {
-        throw new NotYetImplementedException();
+        List<String> previousCmd = Arrays.asList(this.jLineHistory.get(this.jLineHistory.last()).split(" "));
+        previousCmd.add("--no-prompt"); 
+        previousCmd.add("no");
+        return ExecHelper.exec(false, previousCmd.toArray(new String[previousCmd.size()]));
     }
 
     @ShellMethod(value = "reply n")
     public String n() throws IOException {
         return no();
     }
-    
+
 }
